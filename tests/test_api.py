ee3942829b3e6969b739f5e59bfd244c1b1fd400
zzzhdanov18 <m2001786@edu.misis.ru>
2023-02-01 20:11:51 +0300

    add redis-cache

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,9 +1,9 @@
+from main import app
 from fastapi.testclient import TestClient
 import os.path
 import sys
 
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
-from main import app
 
 # include()
 
